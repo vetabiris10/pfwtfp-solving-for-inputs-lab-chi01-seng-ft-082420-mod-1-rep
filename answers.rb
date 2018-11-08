4ee9a9a9007ce1ed5f5612c68587ac2b87d13577
maxwellbenton <maxwellbenton@gmail.com>
2018-11-08 17:27:20 -0500

    added solution

--- answers.rb
+++ answers.rb
@@ -1,27 +1,27 @@
 def what_method_executes_first
   # Return the name of the method that
-
+  "start_game"
 end
 
 def all_valid_inputs
   # Return an Array of all inputs to the first method that will not raise an error
-  
+  [2,3,4,5,6]
 end
 
 def what_is_the_output_type
   # Return the data type that is returned when this code is executed
-
+  "Hash"
 end
 
 def what_method_would_change_if_playing_euchre
   # Euchre is a game that doesn't use a full deck of cards
   # Return the String name of the method that would need to change to adapt
   # this code to playing Euchre
-
+  "create_deck"
 end
 
 def changing_amount_dealt
   # Return the String name of the method that we would need to change to modify
   # the amount of cards dealt to a player's hand
-
+  "setup_game"
 end
