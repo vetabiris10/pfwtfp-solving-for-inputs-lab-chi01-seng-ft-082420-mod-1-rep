d228359dd98d0946ad506bb6d1d83a8fa2d46889
maxwellbenton <maxwellbenton@gmail.com>
2018-11-08 17:26:31 -0500

    set starter code in answers.rb

---
+++ answers.rb
@@ -0,0 +1,27 @@
+def what_method_executes_first
+  # Return the name of the method that
+
+end
+
+def all_valid_inputs
+  # Return an Array of all inputs to the first method that will not raise an error
+  
+end
+
+def what_is_the_output_type
+  # Return the data type that is returned when this code is executed
+
+end
+
+def what_method_would_change_if_playing_euchre
+  # Euchre is a game that doesn't use a full deck of cards
+  # Return the String name of the method that would need to change to adapt
+  # this code to playing Euchre
+
+end
+
+def changing_amount_dealt
+  # Return the String name of the method that we would need to change to modify
+  # the amount of cards dealt to a player's hand
+
+end
